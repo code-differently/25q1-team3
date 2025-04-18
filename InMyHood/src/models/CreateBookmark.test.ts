7e010348aafb81796a6cc88bf1a263c5b407f22b
nia-source <npack251@gmail.com>
2025-04-18 15:00:13 -0400

    feat: implement user roles and bookmark creation logic (#8)
    * feat: implement user roles and bookmark creation logic
    
    * Rename Bookmark.test.ts to CreateBookmark.test.ts
    
    ---------
    
    Co-authored-by: Davis D. Wendell <davisdwendell@gmail.com>

---
+++ InMyHood/src/models/CreateBookmark.test.ts
@@ -0,0 +1,14 @@
+import { Role } from "./Role";
+import { User } from "./User";
+
+const kid = new User("Timmy", Role.YOUTH);
+const parent = new User("Mom", Role.PARENT);
+const liaison = new User("Helper", Role.LIAISON);
+
+createBookmark(kid, { title: "Cool Program" }); // works!
+createBookmark(parent, { title: "Another One" }); // works!
+createBookmark(liaison, { title: "Oops" }); // ❌ will throw an error!
+
+function createBookmark(kid: User, arg1: { title: string; }) {
+    throw new Error("Function not implemented.");
+}
