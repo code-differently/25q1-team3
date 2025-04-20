c7c7a1f6a7bdd1e9dd3eef061a4adf7c32496e37
Ezra Nyabuti <ezramakini123@gmail.com>
2025-04-20 17:23:37 -0400

    feat: refactors program model tests and enhance filtering functionality (#18)
    * feat:initialize package-lock.json for InMyHood project and added Program files
    
    * feat: add package-lock.json for InMyHood project
    
    * chore: remove node_modules from tracking
    
    * chore: re-apply .gitignore and remove ignored files
    
    * feat: adds Github Actions Workflow for test, build
    
    * Update package-lock.json
    
    * feat: refactors bookmark creation logic and update program model types
    
    * feat: refactor program model tests and enhance filtering functionality

---
+++ InMyHood/src/models/CreateBookmark.test.ts
@@ -0,0 +1,37 @@
+import { createBookmark } from "./CreateBookmark";
+import { Role } from "./Role";
+import { User } from "./User";
+
+describe("createBookmark", () => {
+    let user: User;
+    
+    beforeEach(() => {
+        user = {
+        id: 1,
+        name: "John Doe",
+        role: Role.YOUTH,
+        bookmarks: [],
+        savedPrograms: [],
+        getBookmarks: function () {
+            return this.bookmarks;
+        },
+        addBookmark: function (bookmark) {
+            this.bookmarks.push(bookmark);
+        }
+        };
+    });
+    
+    it("should create a bookmark for a user", () => {
+        const data = { url: "https://example.com" };
+        const result = createBookmark(user, data);
+        expect(result).toEqual(data);
+    });
+    
+    it("should throw an error if the user is a liaison", () => {
+        user.role = Role.LIAISON;
+        const data = { url: "https://example.com" };
+        expect(() => createBookmark(user, data)).toThrow(
+        "Unauthorized: Liaisons can’t create bookmarks!"
+        );
+    });
+});
