89795cd63f4595f0bd28c128ab2029c513ed78fa
Khayla Saunders <kksaunders251@gmail.com>
2025-04-20 08:02:16 +0000

    feat:  Add initial unit tests for User class

--- InMyHood/src/models/User.ts
+++ InMyHood/src/models/User.ts
@@ -1,9 +1,5 @@
 import { Role } from "./Role";
 import { Program } from "./Program";
-import {
-  ProgramNotFoundException,
-  BookmarkAlreadyExistsException,
-} from "../utils/CustomExceptions";
 
 export class User {
   username: string;
@@ -17,25 +13,5 @@ export class User {
   getBookmarks(): Program[] {
     return this.bookmarks;
   }
-  // // Add a bookmark
-  // addBookmark(program: Program): void {
-  //   const alreadyBookmarked = this.bookmarks.find(p => p.id === program.id);
-  //   if (alreadyBookmarked) {
-  //     throw new BookmarkAlreadyExistsException(`Program '${program.title}' is already bookmarked.`);
-  //   }
-  //   this.bookmarks.push(program);
-  // }
 
-  // removeBookmark(programId: number): void {
-  //   const index = this.bookmarks.findIndex(p => p.id === programId);
-  //   if (index === -1) {
-  //     throw new ProgramNotFoundException(`Program with ID '${programId}' not found in bookmarks.`);
-  //   }
-  //   this.bookmarks.splice(index, 1);
-  // }
-
-  // // List bookmarks
-  // getBookmarks(): Program[] {
-  //   return this.bookmarks;
-  // }
 }
