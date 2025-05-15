909fac298fd4a24e7f6faaedbdcec7773424eee8
enyabuti <ezramakini123@gmail.com>
2025-05-15 01:39:30 +0000

    Cleans up unnecessary files and ensured compatibility with the new monorepo structure.

--- program-finder/frontend/vite.config.ts
+++ program-finder/frontend/vite.config.ts
@@ -19,12 +19,9 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-        },
+          vendor: ['react', 'react-dom', 'react-router-dom']
+        }
       }
-    
-    proxy: {
-      '/api': 'http://localhost:4000'
     }
   }
 });
