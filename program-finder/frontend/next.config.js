ee57f312e44f37d0be502c26ae78ec0109bcbd7e
enyabuti <ezramakini123@gmail.com>
2025-05-16 02:38:55 +0000

    chore: add .env file for environment variable configuration and clean up next.config.js formatting

--- program-finder/frontend/next.config.js
+++ program-finder/frontend/next.config.js
@@ -2,12 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // If you're using API routes, you might want to configure the API proxy
+  
+  
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:3001/api/:path*', // Adjust this to your backend URL
+        destination: 'http://localhost:3001/api/:path*',
       },
     ]
   },
