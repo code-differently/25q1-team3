0e3aa40399b479280dcb2650639afa0a0cc45b3c
Khayla Saunders <kksaunders251@gmail.com>
2025-05-19 16:09:00 -0400

    feat: adds suspense to the page specifically and not the layout (#50)

--- program-finder/frontend/src/app/programs/page.tsx
+++ program-finder/frontend/src/app/programs/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { ProgramData } from '../../interfaces/ProgramData';
 import { SearchBar } from '../../components/SearchBar';
