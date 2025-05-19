1ae806c285892aaf7912846d8be60c5b62e8f3a6
Khayla Saunders <kksaunders251@gmail.com>
2025-05-19 15:46:22 -0400

    feat: adds suspense to layout (#48)

--- program-finder/frontend/src/app/layout.tsx
+++ program-finder/frontend/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import Script from 'next/script'
-import { useEffect } from 'react'
+import { Suspense, useEffect } from 'react'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -14,6 +14,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+    <Suspense fallback={<h1>Loading...</h1>}>
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
@@ -32,5 +33,6 @@ export default function RootLayout({
         <Script src="/assets/js/main.js" />
       </body>
     </html>
+    </Suspense>
   )
 } 
