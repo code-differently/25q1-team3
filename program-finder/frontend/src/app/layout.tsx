26ed78bde21b03ef924d3e15ff923b347a497dfd
NelltheWiz <chutt251@gmail.com>
2025-05-20 09:45:42 -0400

    chore: reverting changes to layout.tsx

--- program-finder/frontend/src/app/layout.tsx
+++ program-finder/frontend/src/app/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
 import Script from 'next/script'
-import { Suspense, useEffect } from 'react'
+
 import './globals.css'
 
+const inter = Inter({ subsets: ['latin'] })
+
 export const metadata: Metadata = {
-  title: 'CYPHER - Program Finder',
-  description: 'Find Local Programs for Youth in Your Community',
+  title: 'Program Finder',
+  description: 'Find programs in your area',
 }
 
 export default function RootLayout({
@@ -14,23 +17,16 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+
     <html lang="en">
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
         <link rel="stylesheet" href="/assets/css/main.css" />
+        <link rel="stylesheet" href="/assets/css/fontawesome-all.min.css" />
       </head>
-      <body className="is-preload">
+      <body className={inter.className}>
         {children}
-
-        {/* Scripts */}
-        <Script src="/assets/js/jquery.min.js" />
-        <Script src="/assets/js/jquery.dropotron.min.js" />
-        <Script src="/assets/js/jquery.scrollex.min.js" />
-        <Script src="/assets/js/browser.min.js" />
-        <Script src="/assets/js/breakpoints.min.js" />
-        <Script src="/assets/js/util.js" />
-        <Script src="/assets/js/main.js" />
       </body>
     </html>
+
   )
 } 
